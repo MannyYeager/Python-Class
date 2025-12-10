c9f681b80cb78ca4ed56db7a2fd8ddb1ec79618f
MannyYeager <manfredayettey15@gmail.com>
2025-12-10 23:02:34 +0000

    Python Assignment 1

---
+++ main.py
@@ -0,0 +1,15 @@
+playlist =["Cirlces", "No One", "Another Love", "Prom", "Snooze"]
+
+top_artists =("Adele", "Alicia Keys", "John Micheal Howell", "SZA" )
+
+my_music = (playlist, top_artists)
+
+print(len(my_music))
+
+print(playlist[2])
+
+playlist[0] = "Perfect"
+
+playlist.append("Blinding Lights")
+
+print(playlist)
