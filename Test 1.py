f19ec2ae068846be81c434df93807128b4ddefd4
MannyYeager <manfredayettey15@gmail.com>
2025-12-12 14:44:10 +0000

    Movie age verification

---
+++ Test 1.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can watch any movie(Including Aldult Rated Movies)")
+elif age >= 13 and age < 18:
+    print("You can watch PG-13 and lower-rated movies")    
+elif age >= 7 and age < 13:
+    print("You can watch G and PG-rated movies")    
+else:
+    print("You should watch only G-rated(General Audience) movies")
+        
